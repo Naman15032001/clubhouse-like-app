192e98ce70b953ca6fb6b074deb7fa272e2dd6de
Naman15032001 <namanjain15032001@gmail.com>
2022-11-21 20:16:55 +0530

    otp service and db service inital commit

---
+++ backend/controllers/auth-controller.js
@@ -0,0 +1,112 @@
+const hashService = require("../services/hash-service");
+const otpService = require("../services/otp-service");
+const userService = require("../services/user-service");
+
+
+class AuthController {
+
+    async sendOtp(req, res) {
+        //logic
+
+        const {
+            phone
+        } = req.body;
+
+        if (!phone) {
+            res.status(400).json({
+                message: "Phone field is required"
+            })
+        }
+
+        const otp = await otpService.generateOtp();
+
+        const ttl = 1000 * 60 * 2 * 1000000;
+
+        const expires = Date.now() + ttl;
+
+        const data = `${phone}.${otp}.${expires}`;
+
+        const hash = hashService.hashOtp(data);
+
+        //send otp
+        try {
+            await otpService.sendBySms(phone, otp);
+            res.json({
+                hash: `${hash}.${expires}`,
+                phone
+            })
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                message: 'message sending failed'
+            })
+        }
+
+    }
+
+    async verifyOtp(req, res) {
+
+        const {
+            otp,
+            hash,
+            phone
+        } = req.body;
+
+        if (!otp || !hash || !phone) {
+            res.status(400).json({
+                message: "All fields are required"
+            })
+        }
+
+        const [hashedOtp, expires] = hash.split(".");
+
+        if (Date.now() > +expires) {
+            res.status(400).json({
+                message: "Otp expired"
+            })
+        }
+
+        const data = `${phone}.${otp}.${expires}`;
+
+        const isValid = otpService.verifyOtp(hashedOtp, data);
+
+        if (!isValid) {
+            res.status(400).json({
+                message: "Invalid OTP"
+            })
+        }
+
+        let user;
+
+        let accessToken;
+
+        let refreshToken;
+
+        try {
+            user = await userService.findUser({
+                phone
+            })
+            if (!user) {
+                user = await userService.createUser({
+                    phone
+                });
+            }
+        } catch (err) {
+            console.log(err);
+
+            res.status(500).json({
+                message: "Server error"
+            })
+        }
+
+
+
+
+
+
+
+
+    }
+}
+
+module.exports = new AuthController();
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const DbConnect = require('./database');
+const router = require('./routes')
+
+
+
+const app = express();
+
+const PORT = process.env.PORT || 5500;
+
+DbConnect()
+
+app.use(express.json())
+
+app.use(router);
+
+app.get('/', (req, res) => {
+    res.send('Hello from express');
+})
+
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
